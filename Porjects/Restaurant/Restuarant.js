45c92ce8b2d5d03509d8df98dcd894d29f35b26c
nikitas98-kr <nikita.sadekar98@gmail.com>
2025-10-04 19:34:10 +0530

    Delete Porjects directory

--- Porjects/Restaurant/Restuarant.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector("#menu-icon");
-let navbar = document.querySelector(".navlist");
-
-menu.onclick = () => {
-  menu.classList.toggle("bx-x");
-  navbar.classList.toggle("open");
-};
