3c2866f8973be4e104b35e3df6d7c8b9877b7091
Nikita Sadekar <nikitasadekar98@example.com>
2025-10-04 18:51:45 +0530

    Initial commit

--- portfolio.js
+++ portfolio.js
@@ -8,27 +8,27 @@ menu.onclick = () => {
 
 window.onscroll = () => {
   menu.classList.remove("fa-times");
-  header.classList.toggle("active");
+  header.classList.remove("active");
 };
 
-// let cursor1 = document.querySelector(".cursor-1");
-// let cursor2 = document.querySelector(".cursor-2");
+let cursor1 = document.querySelector(".cursor-1");
+let cursor2 = document.querySelector(".cursor-2");
 
-// window.onmousemove = (e) => {
-//   cursor1.style.top = e.pageY + "px";
-//   cursor1.style.left = e.pageX + "px";
-//   cursor2.style.top = e.pageY + "px";
-//   cursor2.style.left = e.pageX + "px";
-// };
+window.onmousemove = (e) => {
+  cursor1.style.top = e.pageY + "px";
+  cursor1.style.left = e.pageX + "px";
+  cursor2.style.top = e.pageY + "px";
+  cursor2.style.left = e.pageX + "px";
+};
 
-// let links = document.querySelectorAll("a").forEach((links) => {
-//   links.onmouseenter = () => {
-//     cursor1.classList.add("active");
-//     cursor2.classList.add("active");
-//   };
+let links = document.querySelectorAll("a").forEach((links) => {
+  links.onmouseenter = () => {
+    cursor1.classList.add("active");
+    cursor2.classList.add("active");
+  };
 
-//   links.onmouseleave = () => {
-//     cursor1.classList.remove("active");
-//     cursor2.classList.remove("active");
-//   };
-// });
+  links.onmouseleave = () => {
+    cursor1.classList.remove("active");
+    cursor2.classList.remove("active");
+  };
+});
